00e84abe651a8c051440403465c8e2803bc2aa5a
kulkaa <ajaykumar.kulkarni@dell.com>
2020-11-22 20:47:20 +0530

    first commit

---
+++ src/test/ChandyMishrahaasORtest.cpp
@@ -0,0 +1,18 @@
+/*
+ * ChandyMishrahaasORtest.cpp
+ *
+ *  Created on: Nov 22, 2020
+ *      Author: kulkaa
+ */
+
+#include "ChandyMishrahaasORtest.h"
+
+ChandyMishrahaas_OR_test::ChandyMishrahaas_OR_test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ChandyMishrahaas_OR_test::~ChandyMishrahaas_OR_test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/test/ChandyMishrahaasORtest.h
@@ -0,0 +1,17 @@
+/*
+ * ChandyMishrahaasORtest.h
+ *
+ *  Created on: Nov 22, 2020
+ *      Author: kulkaa
+ */
+
+#ifndef TEST_CHANDYMISHRAHAASORTEST_H_
+#define TEST_CHANDYMISHRAHAASORTEST_H_
+
+class ChandyMishrahaas_OR_test {
+public:
+	ChandyMishrahaas_OR_test();
+	virtual ~ChandyMishrahaas_OR_test();
+};
+
+#endif /* TEST_CHANDYMISHRAHAASORTEST_H_ */
